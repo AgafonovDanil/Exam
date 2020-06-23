addd23560514851e4b8aaf3e05b23a0f6e21ffc2
danil <danil@DESKTOP-B5464OT>
2020-06-23 18:56:52 +0500

    Добавьте файлы проекта.

---
+++ 2_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = File.ReadAllLines("file.txt");
+            string[] textOut = new string[text.Length];
+            for (int i=0;i<text.Length;i++)
+            {
+                string[] textI = text[i].Split();
+                textI[0] = textI[0]+"?";
+                for (int j =0;j<textI.Length;j++)
+                {
+                    textOut[i] = textOut[i] + textI[j] + " ";
+                }
+                Console.WriteLine(textOut[i]);
+            }
+            Console.ReadLine();    
+        }
+    }
+}
